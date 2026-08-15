8062a2eee6e4d032f7c9e11955716f5da1e3ee4a
Slowpool <nevznachay02@gmail.com>
2026-08-15 18:22:58 +0300

    init 2

---
+++ protected_resource/src/main/java/com/swetlokognatsk/protected_resource/ProtectedResourceApplication.java
@@ -0,0 +1,13 @@
+package com.swetlokognatsk.protected_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectedResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectedResourceApplication.class, args);
+	}
+
+}
---
+++ protected_resource/src/test/java/com/swetlokognatsk/protected_resource/ProtectedResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swetlokognatsk.protected_resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtectedResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
