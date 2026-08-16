bb16b194eb0bd5c5074f5c6e094c37ec84062620
Slowpool <nevznachay02@gmail.com>
2026-08-16 17:40:13 +0300

    draft compose.yaml

--- authorization_server/src/main/java/com/swetlokognatsk/authorization_server/AuthorizationServerApplication.java
+++ authorization_server/src/main/java/com/swetlokognatsk/authorization_server/AuthorizationServerApplication.java
@@ -2,12 +2,21 @@ package com.swetlokognatsk.authorization_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@RestController
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
 	}
 
 }
