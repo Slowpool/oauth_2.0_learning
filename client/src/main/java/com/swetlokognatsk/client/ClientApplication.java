e4146f63e8b112b4b81896310e4365b904dbcc4d
Slowpool <nevznachay02@gmail.com>
2026-08-22 08:32:20 +0300

    favicon, sign in redirect

--- client/src/main/java/com/swetlokognatsk/client/ClientApplication.java
+++ client/src/main/java/com/swetlokognatsk/client/ClientApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+import org.thymeleaf.Thymeleaf;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @RestController
@@ -16,13 +18,34 @@ public class ClientApplication {
 
 	@RequestMapping("/")
 	public String home() {
+		// TODO use thymeleaf
 		var content = "<h1>Hello ClientApplication</h1>" +
+			"<h1>access token value: %s</h1>" + 
+			"<h1>scope value: %s</h1>" + 
+			"<h1></h1>" + 
+			"<h1></h1>" + 
+			
+			// "<button type=\"button\" onclick=\"window.open('%s', '_blank');\">" +
 			"<button type=\"button\" onclick=\"window.location.href='%s';\">" +
-						"Sign in via GipHub" +
+				"<h1>Sign in via GipHub</h1>" +
+			"</button>" +
+
+			"............." +
+
+			"<button type=\"button\" onclick=\"window.open('%s');\">" +
+				"<h1>Get protected resource</h1>" +
 			"</button>";
-		var authEndpointURI = URIBuilder.buildAuthorizationURI();
-		content = String.format(content, authEndpointURI);
+		var getAuthEndpointURI = "/sing-in-via-gip-hub";
+		String accessToken = null;
+		String scope = null;
+		var protectedResourceUri = URIBuilder.buildProtectedResourceURI();
+		content = content.formatted(accessToken, scope, getAuthEndpointURI, protectedResourceUri);
 		return content;
 	}
 
+	@RequestMapping("/sing-in-via-gip-hub")
+	public RedirectView singInViaGipHub() {
+		var authEndpointURI = URIBuilder.buildAuthorizationURI();
+		return new RedirectView(authEndpointURI);
+	}
 }
--- client/src/main/java/com/swetlokognatsk/client/URIBuilder.java
+++ client/src/main/java/com/swetlokognatsk/client/URIBuilder.java
@@ -1,25 +1,37 @@
 package com.swetlokognatsk.client;
 
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.util.UrlPathHelper;
 
 public class URIBuilder {
 
-    public static String buildAuthorizationURI() {
-        var baseUri = AuthorizationServer.getAuthorizationEndpoint();
-        var authorizationUri = buildURI(baseUri, null, "");
-        return authorizationUri;
-    }
-
-    private static String buildURI(String base, Map<String,String> options, String hash) {
+    private static String buildURI(String base, Map<String, String> options, String hash) {
         var uri = UriComponentsBuilder.fromUriString(base);
         uri.replaceQuery("");
         for (var option : options.keySet()) {
             uri.queryParam(option, options.get(option));
         }
-        uri.fragment(hash);
-        return uri.toUriString();
+        if (hash != null) {
+            uri.fragment(hash);
+        }
+        return uri.encode().toUriString();
     }
+
+    public static String buildAuthorizationURI() {
+        var baseUri = AuthorizationServer.getAuthorizationEndpoint();
+        var options = new HashMap<String, String>();
+        options.put("response_type", "code");
+        options.put("client_id", Client.getId());
+        options.put("redirect_uri", Client.getRedirectURI());
+        var authorizationUri = buildURI(baseUri, options, null);
+        return authorizationUri;
+    }
+
+    public static String buildProtectedResourceURI() {
+        return "nope";
+    }
+
 }
