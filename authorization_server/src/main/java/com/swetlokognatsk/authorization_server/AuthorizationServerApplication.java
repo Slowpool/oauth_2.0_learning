92f66aa362868cde12643d0d4da824be033807ae
Slowpool <nevznachay02@gmail.com>
2026-09-02 17:50:15 +0300

    /authorize draft, checking the client_id and redirect_uri

--- authorization_server/src/main/java/com/swetlokognatsk/authorization_server/AuthorizationServerApplication.java
+++ authorization_server/src/main/java/com/swetlokognatsk/authorization_server/AuthorizationServerApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.swetlokognatsk.authorization_server.daos.ClientsDao;
+import com.swetlokognatsk.authorization_server.models.Client;
+import com.swetlokognatsk.authorization_server.models.RedirectUri;
 import com.swetlokognatsk.authorization_server.ports.Database;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 // TODO create all base scenarios in oauth and write logs from each service in demonstration purposes
 @SpringBootApplication
@@ -15,15 +21,45 @@ import com.swetlokognatsk.authorization_server.ports.Database;
 @RestController
 public class AuthorizationServerApplication {
 
+	private static final String HOME = "/";
+	private static final String AUTHORIZATION_ENDPOINT = "/authorize";
+
 	private static ApplicationContext ctx;
 
 	public static void main(String[] args) {
 		ctx = SpringApplication.run(AuthorizationServerApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping(HOME)
 	public String home() {
 		return "<h1>Hello AuthorizationServerApplication</h1>";
+	}
+
+	@GetMapping(AUTHORIZATION_ENDPOINT)
+	public ResponseEntity<?> authorize(@RequestParam(name = "client_id") final String clientId, @RequestParam(name = "redirect_uri") final String redirectUri) {
+		var database = ctx.getBean(Database.class);
+		try {
+			var client = database.getClient(clientId);
+			validateClient(client, redirectUri);//, clientSecret);
+		}
+		catch (ClientNotFoundException e) {
+			return ResponseEntity.status(404).body("unknown client: %s".formatted(clientId));
+		}
+		catch (InvalidRedirectUriException e) {
+			return ResponseEntity.unprocessableContent().body("incorrect redirectUri: %s".formatted(redirectUri));
+		}
+
+		return ResponseEntity.ok("/authorize, clientId: %s".formatted(clientId));
+	}
+
+	private void validateClient(final Client client, final String receivedRedirectUri) throws InvalidRedirectUriException {
+		var clientRedirectUris = client.getRedirectUris()
+			.stream()
+			.map((RedirectUri redirectUri) -> redirectUri.uri)
+			.toList();
+		if (!clientRedirectUris.contains(receivedRedirectUri)) {
+			throw new InvalidRedirectUriException();
+		}
 	}
 
 	@RequestMapping("/clients-test")
---
+++ authorization_server/src/main/java/com/swetlokognatsk/authorization_server/InvalidRedirectUriException.java
@@ -0,0 +1,5 @@
+package com.swetlokognatsk.authorization_server;
+
+public final class InvalidRedirectUriException extends Exception {
+
+}
